0662a18269c7540b349d57dbb5f72e52b212b6a9
Fekreselassie <fekruson@gmail.com>
2025-09-07 01:36:13 +0300

    fix views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,8 @@
 
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book
+from .models import Library
 
 # Function-based view to list all books
 def list_books(request):
