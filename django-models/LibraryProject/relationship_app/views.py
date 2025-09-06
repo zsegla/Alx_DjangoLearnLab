06a2121017a26f13ff269dfb83a8b6d2588692ed
Fekreselassie <fekruson@gmail.com>
2025-09-07 01:35:24 +0300

    update views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -11,7 +11,7 @@ def list_books(request):
 # Class-based view to display library details
 class LibraryDetailView(DetailView):
 	model = Library
-	template_name = 'library_detail.html'
+	template_name = 'relationship_app/library_detail.html'
 	context_object_name = 'library'
 
 # Create your views here.
