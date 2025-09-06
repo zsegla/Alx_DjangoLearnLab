014e5fd137ba5bb81bbcf43a43d79fab3f9d4fd3
Fekreselassie <fekruson@gmail.com>
2025-09-07 01:37:10 +0300

    again views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
 
