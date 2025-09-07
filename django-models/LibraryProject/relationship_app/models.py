2d45a079952f3da9e37ce82c1283a1d995047611
Fekreselassie <fekruson@gmail.com>
2025-09-07 14:26:38 +0300

    task 3

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,5 +1,29 @@
 
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+# UserProfile model to extend User with role
+class UserProfile(models.Model):
+	ROLE_CHOICES = [
+		('Admin', 'Admin'),
+		('Librarian', 'Librarian'),
+		('Member', 'Member'),
+	]
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+	def __str__(self):
+		return f"{self.user.username} ({self.role})"
+
+# Signal to create UserProfile automatically
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+	if created:
+		UserProfile.objects.create(user=instance, role='Member')
+
+from django.db import models
 
 # Author Model
 class Author(models.Model):
