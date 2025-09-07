f29b90716485f1224009ad69551b670055459db2
Fekreselassie <fekruson@gmail.com>
2025-09-07 14:14:42 +0300

    importing forms

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.detail import DetailView
